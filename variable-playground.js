0ff831564a7b77866b69deae6b3588396542edda
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-06-08 12:46:43 +0300

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name:'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var array = [15, 17];
+
+function updateArray(arr) {
+	arr = [15, 17];
+	
+	arr = arr.push(18)
+	debugger;
+};
+
+updateArray(array);
+console.log(array);
