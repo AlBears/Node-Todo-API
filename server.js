24bb9f0c4f4fe9eda5e8f14eb44528f2120437d4
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-06-08 12:45:16 +0300

    Added ?completed filter

--- server.js
+++ server.js
@@ -12,9 +12,17 @@ app.use(bodyParser.json());
 app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
-// GET TODO
+// GET TODO /todos?completed=true
 app.get('/todos', function (req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){ 
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+	res.json(filteredTodos);
 });
 // GET TODO BY ID
 app.get('/todos/:id', function (req, res){
