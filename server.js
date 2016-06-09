a7e79394f82d5b107c0be78beccbd4f73866a35d
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-06-09 08:48:14 +0300

    Final Static Todos

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
-// GET TODO /todos?completed=true
+// GET TODO /todos?completed=true&q=house
 app.get('/todos', function (req, res){
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -22,6 +22,13 @@ app.get('/todos', function (req, res){
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){ 
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q) > -1;
+		});
+	}
+
 	res.json(filteredTodos);
 });
 // GET TODO BY ID
