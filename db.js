b8f98eee6de3a814ba6da4761614091d998dc9f0
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-06-15 19:46:27 +0300

    Adding DELETE /users/login"

--- db.js
+++ db.js
@@ -6,11 +6,11 @@ if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
 	});
-}else {
+} else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
 }
 
 
@@ -18,6 +18,7 @@ var db = {};
 
 db.todo = sequelize.import(__dirname +'/models/todo.js');
 db.user = sequelize.import(__dirname +'/models/user.js');
+db.token = sequelize.import(__dirname +'/models/token.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
